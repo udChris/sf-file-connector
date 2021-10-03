c8aa6145b495a774d4e8b3aafdaf02a9bd0b1ada
ZackMinott <zacharyminott1997@gmail.com>
2021-10-03 08:30:22 -0700

    added in logging for createFile Post request

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,10 +1,11 @@
 
 exports.createFile = async (req, res) => {
     console.log(req.body);
-    res.status(201).json({
-        status: 'success',
-        data: {
-            fileData : req.body
-        }
-    })
+    console.log('IN HERE');
+    // res.status(201).json({
+    //     status: 'success',
+    //     data: {
+    //         fileData : req.body
+    //     }
+    // })
 }
