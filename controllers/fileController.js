f1687084ab0eab4c193178b54c1d3ea51fc99f38
ZackMinott <zacharyminott1997@gmail.com>
2021-10-03 08:59:27 -0700

    added in res.return status

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,11 +1,9 @@
 
 exports.createFile = async (req, res) => {
-    console.log(req.body);
-    console.log('IN HERE');
-    // res.status(201).json({
-    //     status: 'success',
-    //     data: {
-    //         fileData : req.body
-    //     }
-    // })
+    res.status(201).json({
+        status: 'success',
+        data: {
+            fileData : req.body
+        }
+    })
 }
