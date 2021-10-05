340ba32bd6c20c0561cc86dcbaca4c5a100fda61
CampUS-Stack <82190690+CampUS-Stack@users.noreply.github.com>
2021-10-04 23:03:45 -0400

    Created POST endpoint in /azure to handle creation of new file. Updated some AzureAPIService logic

--- routes/azure.js
+++ routes/azure.js
@@ -4,11 +4,24 @@ const router = express.Router();
 const azureServices = require("../services/azure");
 
 
-// region GET
 router.get("/", [], async (req, res) => {
 
     let filesToSend = await azureServices.getFilesFromDirectory(req.body.directoryName);
     return res.send({...filesToSend});
+});
+
+router.post("/", [], async (req, res) => {
+
+    let filesToSend = await azureServices.createNewFile(req.body.name,req.body.directory,req.body.data);
+
+    console.log(filesToSend);
+
+    if(filesToSend){
+        return res.status(200).send('SUCCESS');
+    }
+    else{
+        return res.status(400).send('FAILED');
+    }
 });
 
 module.exports = router;
