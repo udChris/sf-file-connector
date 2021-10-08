486a587f67463ea062cddf68f6263f6c42a6b9e6
ZackMinott <zacharyminott1997@gmail.com>
2021-10-07 19:32:45 -0700

    reverted back to Chris's code and commented out my code

--- services/azure.js
+++ services/azure.js
@@ -8,8 +8,8 @@ const { ShareServiceClient } = require("@azure/storage-file-share");
 const connStr = "https://udfileuploader.file.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2023-09-26T09:59:03Z&st=2021-09-26T01:59:03Z&sip=98.230.182.225&spr=https&sig=z2wjbu%2BlClJEpmGOT5R6NQx9zAICSNgo3B%2BTaMWz3R4%3D";
 
 // both lines created by Zack
-const serviceClient = ShareServiceClient.fromConnectionString(connStr);
-const shareName = 'udfileclient';
+// const serviceClient = ShareServiceClient.fromConnectionString(connStr);
+// const shareName = 'udfileclient';
 
 const createFileShare = async (name) => {
 
@@ -27,18 +27,7 @@ const createNewDirectory = async (name) => {
 
 const createNewFile = async (name, directory, fileContent) => {
 
-    const shareClient = serviceClient.getShareClient(shareName);
-    console.log('Got past share client');
-    const directoryClient = shareClient.getDirectoryClient(name);
-    console.log('Got past directory');
-    const fileClient = directoryClient.getFileClient(name);
-    console.log('Got past get file client')
-    await fileClient.create(fileContent.length);
-    console.log('created file');
-    await fileClient.uploadRange(fileContent, 0, content.length);
-    console.log('finally');
-
-    // const shareClient = new ShareServiceClient(connStr);
+    // const shareClient = serviceClient.getShareClient(shareName);
     // console.log('Got past share client');
     // const directoryClient = shareClient.getDirectoryClient(name);
     // console.log('Got past directory');
@@ -48,6 +37,17 @@ const createNewFile = async (name, directory, fileContent) => {
     // console.log('created file');
     // await fileClient.uploadRange(fileContent, 0, content.length);
     // console.log('finally');
+
+    const shareClient = new ShareServiceClient(connStr);
+    console.log('Got past share client');
+    const directoryClient = shareClient.getDirectoryClient(name);
+    console.log('Got past directory');
+    const fileClient = directoryClient.getFileClient(name);
+    console.log('Got past get file client')
+    await fileClient.create(fileContent.length);
+    console.log('created file');
+    await fileClient.uploadRange(fileContent, 0, content.length);
+    console.log('finally');
 }
 
 const getFilesFromDirectory = async (shareName, directoryName) => {
