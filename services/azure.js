b042d4c5ad698dedd28aebcdbf3a0d6edef9b896
ZackMinott <zacharyminott1997@gmail.com>
2021-10-10 16:26:37 -0700

    updated connection string

--- services/azure.js
+++ services/azure.js
@@ -6,7 +6,7 @@
 const { ShareServiceClient } = require("@azure/storage-file-share");
 
 // const connStr = "https://udfileuploader.file.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2023-09-26T09:59:03Z&st=2021-09-26T01:59:03Z&sip=98.230.182.225&spr=https&sig=z2wjbu%2BlClJEpmGOT5R6NQx9zAICSNgo3B%2BTaMWz3R4%3D";
-const connStr = '?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-11T07:20:10Z&st=2021-10-10T23:20:10Z&sip=3.210.192.5&spr=https&sig=%2FgpUzx9acXksb7HHy1R9odIp2m%2BO2JxBwgO%2F8HauVUo%3D';
+const connStr = 'https://udfileuploader.file.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-11T07:20:10Z&st=2021-10-10T23:20:10Z&sip=3.210.192.5&spr=https&sig=%2FgpUzx9acXksb7HHy1R9odIp2m%2BO2JxBwgO%2F8HauVUo%3D';
 
 // both lines created by Zack
 // const serviceClient = ShareServiceClient.fromConnectionString(connStr);
