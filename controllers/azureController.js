42c4c3dc8002c36a7538db6da57ea4f9a0a6985d
ZackMinott <zacharyminott1997@gmail.com>
2021-10-11 18:03:51 -0700

    update router logic with middleware and sas token

--- controllers/azureController.js
+++ controllers/azureController.js
@@ -13,16 +13,27 @@ exports.get = async (req, res) => {
 }
 
 exports.post = async (req, res) => {
-    try {
-        await azureServices.createNewFile(req.body.name,req.body.directory,req.files[0])
-        res.status(200).json({
-            status: "success",
-            message : "Successfully uploaded the files!"
-        })
-    } catch(e) {
-        res.status(400).json({
-            status: "FAILED",
-            message : e
-        })
+    let filesToSend = await azureServices.createNewFile(req.body.name, req.body.directory, req.files[0]);
+
+    console.log(filesToSend);
+
+    if (filesToSend) {
+        return res.status(200).send('SUCCESS');
+    } else {
+        return res.status(400).send('FAILED');
     }
+
+
+    // try {
+    //     await azureServices.createNewFile(req.body.name,req.body.directory,req.files[0])
+    //     res.status(200).json({
+    //         status: "success",
+    //         message : "Successfully uploaded the files!"
+    //     })
+    // } catch(e) {
+    //     res.status(400).json({
+    //         status: "FAILED",
+    //         message : e
+    //     })
+    // }
 }
