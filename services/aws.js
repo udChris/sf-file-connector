35adab7b7efec1e0f105ceeb37649bc5359581be
ZackMinott <zacharyminott1997@gmail.com>
2021-10-15 21:11:44 -0700

    Created routes and implemented new file params for azure file router. Started building AWS file service. So far only created the upload function (will need to refactor). Installed aws-sdk

--- services/aws.js
+++ services/aws.js
@@ -1,29 +1,28 @@
 const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
 
-const accessKeyId = 'AKIA4F4BZU53TUXDMEYE';
-const secretAccessKey = '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';
-const bucketName = 'udfileuploader';
-const region = 'us-west-1';
 
-const s3 = new S3({
-    region,
-    accessKeyId,
-    secretAccessKey
-});
-
-// uploads a file to s3
-exports.upload = (file) => {
-    const fileStream = fs.createReadStream(file.path);
-
-    const uploadParams = {
-        Bucket : bucketName,
-        Body : fileStream,
-        Key : file.filename
-    }
-
-    return s3.upload(uploadParams).promise();
-}
+// const bucketName = 'udfileuploader';
+// const region = 'us-west-1';
+//
+// const s3 = new S3({
+//     region,
+//     accessKeyId,
+//     secretAccessKey
+// });
+//
+// // uploads a file to s3
+// exports.upload = (file) => {
+//     const fileStream = fs.createReadStream(file.path);
+//
+//     const uploadParams = {
+//         Bucket : bucketName,
+//         Body : fileStream,
+//         Key : file.filename
+//     }
+//
+//     return s3.upload(uploadParams).promise();
+// }
 
 // downloads a file from s3
 
